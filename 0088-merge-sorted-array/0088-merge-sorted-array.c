8c5f970e795aea5d371b98caf4378aa87eee53d8
Phan Van Tranh <152046827+PhanVanTranh@users.noreply.github.com>
2026-08-27 20:28:27 +0700

    Time: 0 ms (100%), Space: 10.8 MB (36.94%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -0,0 +1,24 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    nums1Size = m+n;
+
+    for(int i = m; i < nums1Size; i++)
+    {
+       nums1[i] = nums2[i-m];
+    }
+
+    for (int i = 0; i < nums1Size - 1 ; i++)
+    {
+        for(int j = i + 1; j < nums1Size; j++)
+        {
+            if(nums1[i] > nums1[j])
+            {
+                int temp = nums1[i];
+                nums1[i] = nums1[j];
+                nums1[j] = temp;
+            }
+        }
+
+        
+    }
+
+}
